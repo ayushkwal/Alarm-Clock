7115c833f47bfd5d7236543f1cd753d78bcf6c16
ayush <ayush.kwal@gmail.com>
2020-10-25 22:06:26 +0530

    second init

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log("hello")
+// let d = new Date();
+// console.log(d.getHours());
 let set = document.getElementById('btn');
 let hrs = document.getElementById('hrs').value;
 let min = document.getElementById('min').value;
@@ -7,11 +9,18 @@ set.addEventListener('click',setalarm);
 
 function setalarm() {
     // console.log("alarmsetted");
-    let hrs = document.getElementById('hrs').value;
+let hrs = document.getElementById('hrs').value;
 let min = document.getElementById('min').value;
 let body = document.getElementById('body');
 const total = ((hrs*60*60) + (min*60))*1000;
+let d = new Date();
+let cur_hrs = d.getHours();
+console.log(cur_hrs);
+let cur_min = d.getMinutes();
+console.log(cur_min);
 
+const cur = (((hrs-cur_hrs)*60*60)+((min-cur_min)*60))*1000;
+console.log(cur);
 
 
     setTimeout(()=>{
@@ -21,11 +30,11 @@ const total = ((hrs*60*60) + (min*60))*1000;
         let aftertime = document.getElementById('aftertime');
         let clock = document.getElementById('time');
         var audio = document.getElementById('myAudio');
-        console.log(aftertime);
+      //  console.log(aftertime);
         audio.play();
         beforetime.style.display = "none";
         aftertime.style.display = "block";
      
         //Your Alarm function ends -------------------------------------------------------->
-    },total);
+    },cur);
 }
